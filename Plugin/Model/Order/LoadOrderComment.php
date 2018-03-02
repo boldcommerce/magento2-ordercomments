fb57f06e7e52c3cbfa0f0300da1c6735cc07bf9d
Sidney Monk <sidney@boldcommerce.nl>
2018-03-02 11:57:23 +0100

    #15 - feature - add comment to salesOrderRepositoryV1 api

---
+++ Plugin/Model/Order/LoadOrderComment.php
@@ -0,0 +1,55 @@
+<?php
+namespace Bold\OrderComment\Plugin\Model\Order;
+
+use \Magento\Sales\Api\OrderRepositoryInterface;
+use \Magento\Sales\Api\Data\OrderInterface;
+use \Magento\Sales\Model\OrderFactory;
+use \Magento\Sales\Api\Data\OrderExtensionFactory;
+class LoadOrderComment
+{
+    private $orderFactory;
+
+    private $orderExtensionFactory;
+
+    public function __construct(
+        OrderFactory $orderFactory,
+        OrderExtensionFactory $extensionFactory
+    ) {
+        $this->orderFactory = $orderFactory;
+        $this->orderExtensionFactory = $extensionFactory;
+    }
+
+    public function afterGet(
+        OrderRepositoryInterface $subject,
+        OrderInterface $resultOrder
+    ) {
+        $this->setOrderComment($resultOrder);
+        return $resultOrder;
+    }
+
+    public function setOrderComment(OrderInterface $order)
+    {
+        if ($order instanceof \Magento\Sales\Model\Order) {
+            $value = $order->getBoldOrderComment();
+        } else {
+            $temp = $this->getOrderFactory()->create();
+            $temp->load($order->getId());
+            $value = $temp->getBoldOrderComment();
+        }
+
+        $extensionAttributes = $order->getExtensionAttributes();
+        $orderExtension = $extensionAttributes ? $extensionAttributes : $this->getOrderExtensionFactory()->create();
+        $orderExtension->setBoldOrderComment($value);
+        $order->setExtensionAttributes($orderExtension);
+    }
+
+    public function getOrderFactory()
+    {
+        return $this->orderFactory;
+    }
+
+    public function getOrderExtensionFactory()
+    {
+        return $this->orderExtensionFactory;
+    }
+}
