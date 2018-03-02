84350e926bd4c6e54e9305a300e1c59955651bee
Sidney Monk <sidney@boldcommerce.nl>
2018-03-02 09:59:04 +0100

    #15 - enhancement - prevent input from exceeding configured character limit

--- view/frontend/web/js/view/checkout/order-comment-block.js
+++ view/frontend/web/js/view/checkout/order-comment-block.js
@@ -7,6 +7,36 @@ define(
     function ($, Component, ko) {
         'use strict';
 
+        ko.extenders.maxOrderCommentLength = function (target, maxLength) {
+            var timer;
+            var result = ko.computed({
+                read: target,
+                write: function (val) {
+                    if (maxLength > 0) {
+                        clearTimeout(timer);
+                        if (val.length > maxLength) {
+                            var limitedVal = val.substring(0, maxLength);
+                            if (target() === limitedVal) {
+                                target.notifySubscribers();
+                            } else {
+                                target(limitedVal);
+                            }
+                            result.css("_error");
+                            timer = setTimeout(function () { result.css(""); }, 800);
+                        } else {
+                            target(val);
+                            result.css("");
+                        }
+                    } else {
+                        target(val);
+                    }
+                }
+            }).extend({ notify: 'always' });
+            result.css = ko.observable();
+            result(target());
+            return result;
+        };
+
         return Component.extend({
             defaults: {
                 template: 'Bold_OrderComment/checkout/order-comment-block'
@@ -14,10 +44,12 @@ define(
             initialize: function() {
                 this._super();
                 var self = this;
-                this.comment = ko.observable("");
+                this.comment = ko.observable("").extend({maxOrderCommentLength: this.getMaxLength()});
+
                 this.remainingCharacters = ko.computed(function(){
                     return self.getMaxLength() - self.comment().length;
                 });
+
             },
             hasMaxLength: function() {
                 return window.checkoutConfig.max_length > 0;
