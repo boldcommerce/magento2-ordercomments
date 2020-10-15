0b7979cd50cf0d5a7a3d5622281464d3a1d78d01
Jur Hillebrink <jur@boldcommerce.nl>
2020-10-15 11:08:58 +0200

    Added option to show or hide order comment input field in checkout

--- Model/OrderCommentConfigProvider.php
+++ Model/OrderCommentConfigProvider.php
@@ -4,12 +4,15 @@ namespace Bold\OrderComment\Model;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class OrderCommentConfigProvider implements ConfigProviderInterface
 {
     const CONFIG_MAX_LENGTH = 'sales/ordercomments/max_length';
     
     const CONFIG_FIELD_COLLAPSE_STATE = 'sales/ordercomments/collapse_state';
+
+    const CONFIG_SHOW_IN_CHECKOUT = 'sales/ordercomments/show_in_checkout';
 
     /**
      * @var ScopeConfigInterface
@@ -24,8 +27,9 @@ class OrderCommentConfigProvider implements ConfigProviderInterface
     public function getConfig()
     {
         return [
-            'max_length' => (int) $this->scopeConfig->getValue(self::CONFIG_MAX_LENGTH),
-            'comment_initial_collapse_state' => (int) $this->scopeConfig->getValue(self::CONFIG_FIELD_COLLAPSE_STATE)
+            'show_in_checkout' => $this->scopeConfig->isSetFlag(self::CONFIG_SHOW_IN_CHECKOUT, ScopeInterface::SCOPE_WEBSITE),
+            'max_length' => (int) $this->scopeConfig->getValue(self::CONFIG_MAX_LENGTH, ScopeInterface::SCOPE_WEBSITE),
+            'comment_initial_collapse_state' => (int) $this->scopeConfig->getValue(self::CONFIG_FIELD_COLLAPSE_STATE, ScopeInterface::SCOPE_WEBSITE)
         ];
     }
 
