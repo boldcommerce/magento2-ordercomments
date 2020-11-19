ca65f4dc2908185b4f378f0ca5df310ece2d62a5
Neven Kajić <neven.kajic@digitalniplan.hr>
2020-11-19 16:32:13 +0100

    Feature: Admin: Move checkout comment field after payment methods

---
+++ Plugin/Block/Checkout/ModifyLayout.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Bold\OrderComment\Plugin\Block\Checkout;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessor;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class ModifyLayout {
+
+    const CONFIG_MOVE_COMMENT_AFTER_PAYMENT_METHOD = 'sales/ordercomments/move_comment_after_payment_methods';
+
+    /**
+     * ModifyLayout constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param LayoutProcessor $layoutProcessor
+     * @param callable $proceed
+     * @param array<int, mixed> $args
+     * @return array
+     */
+    public function aroundProcess(LayoutProcessor $layoutProcessor, callable $proceed, ...$args)
+    {
+        $jsLayout = $proceed(...$args);
+
+        if ($this->scopeConfig->getValue(self::CONFIG_MOVE_COMMENT_AFTER_PAYMENT_METHOD)) {
+            $this->moveOrderCommentComponentAfterPaymentMethods($jsLayout);
+        }
+
+        return $jsLayout;
+    }
+
+    /**
+     * Make changes on layout tree
+     * move Order comment component after payment methods
+     * @param &$jsLayout
+     */
+    public function moveOrderCommentComponentAfterPaymentMethods(&$jsLayout) {
+        $orderCommentComponentParentReference =& $jsLayout['components']['checkout']['children']
+            ['steps']['children']['billing-step']['children']
+            ['payment']['children']['payments-list']['children']
+            ['before-place-order']['children'];
+
+        // Make copy of component so that we can unset original
+        $orderCommentComponent = (array) $orderCommentComponentParentReference['comment'];
+        unset($orderCommentComponentParentReference['comment']);
+
+        // In case we miss sortOrder make it first element inside afterMethods
+        if (!isset($orderCommentComponent['sortOrder'])) {
+            $orderCommentComponent['sortOrder'] = 0;
+        }
+
+        $jsLayout['components']['checkout']['children']
+        ['steps']['children']['billing-step']['children']
+        ['payment']['children']['afterMethods']['children']['comment'] = $orderCommentComponent;
+    }
+
+}
